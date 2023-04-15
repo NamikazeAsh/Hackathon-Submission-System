66c03a5d49fabb037eab752776b6f71bd4ef5c67
NamikazeAsh <ashwinsatish0@gmail.com>
2023-04-15 19:14:44 +0530

    submission model creation

--- HackathonApp/models.py
+++ HackathonApp/models.py
@@ -18,3 +18,14 @@ class HackathonModel(models.Model):
     enddate = models.DateField()
     reward = models.IntegerField()
     
+class SubmissionModel(models.Model):
+
+    hackathonid = models.ForeignKey(HackathonModel,on_delete=models.CASCADE)
+    title = models.TextField(max_length=100)
+    summary = models.TextField(max_length=100)
+    description = models.TextField(max_length=500)
+    coverimg = models.ImageField(upload_to = "images/coverimages")
+    subimg = models.ImageField(null=True,blank=True,upload_to="images/subimages")
+    sublink = models.URLField(max_length=200,null=True,blank=True)
+    subfile = models.FileField(null=True,blank=True,upload_to="images/subfile")
+    
